f2bcbbc189390bf2fc9196a1444375299ac0b6e6
emanuele.lamalfa@mail.polimi.it <emanuele.lamalfa@mail.polimi.it>
2023-08-11 09:11:02 +0100

    new

--- code.js
+++ code.js
@@ -2,8 +2,7 @@ function get_random_image() {
     var image_array = [
       '/img/lagna.jpeg',
       '/img/lagna1.jpeg',
-      '/img/sangiulio.jpeg',
-      '/img/gatozzo.jpeg'
+      '/img/sangiulio.jpeg'
     ];
     const random_index = Math.floor(Math.random() * image_array.length);
     const selected_image = image_array[random_index];
