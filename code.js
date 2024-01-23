5ccb3db0845704827d8316f5b8eefb6189bbf511
Emanuele La Malfa <emalfa@CSL15744.local>
2024-01-23 09:38:17 +0000

    upload

--- code.js
+++ code.js
@@ -2,7 +2,8 @@ function get_random_image() {
     var image_array = [
       '/img/lagna.jpeg',
       '/img/lagna1.jpeg',
-      '/img/sangiulio.jpeg'
+      '/img/sangiulio.jpeg',
+      '/img/mottarone.jpg'
     ];
     const random_index = Math.floor(Math.random() * image_array.length);
     const selected_image = image_array[random_index];
