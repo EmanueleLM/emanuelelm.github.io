057139fe99244b8ec893462688ead09eb20e9c76
Emanuele La Malfa <emalfa@CSL15744.local>
2025-01-22 18:09:13 +0000

    stuff

--- code.js
+++ code.js
@@ -3,7 +3,8 @@ function get_random_image() {
       '/img/lagna.jpeg',
       '/img/lagna1.jpeg',
       '/img/sangiulio.jpeg',
-      '/img/mottarone.jpg'
+      '/img/mottarone.jpg',
+      'img/gatozzo.jpeg'
     ];
     const random_index = Math.floor(Math.random() * image_array.length);
     const selected_image = image_array[random_index];
